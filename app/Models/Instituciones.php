c667b03102ad821045d0034b0ff5bae45bf7fb47
Tiagotorres37 <Santiagotorresarenas2018@gmail.com>
2023-05-08 21:29:52 -0500

    modelo y migracion insti

---
+++ app/Models/Instituciones.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instituciones extends Model
+{
+    use HasFactory;
+
+    protected $table = 'instituciones';
+
+    protected $fillable = ['id','nombre'];
+}
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -75,6 +75,32 @@
                 </div>
             </a>
         </div>
+
+        <div class="flex flex-row gap-4 my-2">
+            
+            <a href="{{ route('documentos.index') }}" class="bg-white rounded-lg shadow-lg overflow-hidden flex-none w-1/3 flex items-center p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+                </svg>
+                  
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Instituciones</div>
+                    <p class="text-gray-700 text-base">Gestion de instituciones</p>
+                </div>
+            </a>
+
+            <a href="{{ route('epss.index') }}" class="bg-white rounded-lg shadow-lg overflow-hidden flex-none w-1/3 flex items-center p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
+                </svg>
+                  
+                <div class="px-6 py-4">
+                  <div class="font-bold text-xl mb-2">Idiomas</div>
+                  <p class="text-gray-700 text-base">Gestion de idiomas</p>
+                </div>
+            </a>
+
+        </div>
     </div>
 
 @endsection
